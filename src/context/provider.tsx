6480bbb189c932ab26101232d9de709ab047ef13
Pagebakers <eelco@appulse.nl>
2024-02-22 13:57:05 -0300

    4. add react query

--- src/context/provider.tsx
+++ src/context/provider.tsx
@@ -1,17 +1,22 @@
 import { forwardRef } from "react";
-import { SaasProvider } from "@saas-ui/react";
-import {
-  Link,
-  LinkProps,
-  RouterProvider,
-  createRouter,
-} from "@tanstack/react-router";
+import { LinkProps, SaasProvider } from "@saas-ui/react";
+import { RouterProvider, createRouter, Link } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { routeTree } from "../routeTree.gen";
+
+const queryClient = new QueryClient();
 
 // Set up a Router instance
 const router = createRouter({
   routeTree,
+  context: {
+    queryClient,
+  },
+  defaultPreload: "intent",
+  // Since we're using React Query, we don't want loader calls to ever be stale
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  defaultPreloadStaleTime: 0,
 });
 
 // Register things for typesafety
@@ -31,8 +36,10 @@ const LinkComponent = forwardRef<HTMLAnchorElement, Pick<LinkProps, "href">>(
 
 export const Provider = () => {
   return (
-    <SaasProvider linkComponent={LinkComponent}>
-      <RouterProvider router={router} />
-    </SaasProvider>
+    <QueryClientProvider client={queryClient}>
+      <SaasProvider linkComponent={LinkComponent}>
+        <RouterProvider router={router} />
+      </SaasProvider>
+    </QueryClientProvider>
   );
 };
--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -11,9 +11,15 @@
 // Import Routes
 
 import { Route as rootRoute } from './routes/__root'
+import { Route as ProfileImport } from './routes/profile'
 import { Route as IndexImport } from './routes/index'
 
 // Create/Update Routes
+
+const ProfileRoute = ProfileImport.update({
+  path: '/profile',
+  getParentRoute: () => rootRoute,
+} as any)
 
 const IndexRoute = IndexImport.update({
   path: '/',
@@ -28,11 +34,15 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
+    '/profile': {
+      preLoaderRoute: typeof ProfileImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
 // Create and export the route tree
 
-export const routeTree = rootRoute.addChildren([IndexRoute])
+export const routeTree = rootRoute.addChildren([IndexRoute, ProfileRoute])
 
 /* prettier-ignore-end */
